bdcbd0c03fcaa5f81381f50b6bf2eae52820ca83
매그니토 <anyman@afreecatv.com>
2020-03-16 15:07:39 +0900

    db연동

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,19 @@
+import pymysql
 import json
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+
+# MySQL Connection 연결
+conn = pymysql.connect(host='database-1.chilgjy3o3zp.ap-northeast-2.rds.amazonaws.com', user='admin', password='epdlxjqpdltm-1',
+                       db='innodb', charset='utf8')
+
+# Connection 으로부터 Cursor 생성
+curs = conn.cursor()
+
+domain = 'http://www.slrclub.com'
 
 
 def lambda_handler(event, context):
@@ -20,28 +33,40 @@ def lambda_handler(event, context):
 
         num = tr.find('td', class_='list_num').string
         title = tr.find('td', class_='sbj').a.string
+        url = domain + tr.find('td', class_='sbj').a['href']
+        # comment222 = list(tr.find('td', class_='sbj').children)
+        sbj = list(tr.find('td', class_='sbj').children)
+        comment_cnt = str(
+            sbj[len(sbj)-1].string).replace('[', '').replace(']', '').replace(' ', '')
+        comment_cnt = 0 if comment_cnt == '' else comment_cnt
         writer = tr.find('td', class_='list_name').span.string
-        tm = tr.find('td', class_='list_date').string
-        viewCnt = tr.find('td', class_='list_click').string
-        #num = tr.prettify()
+        list_date = tr.find('td', class_='list_date').string
+        view_cnt = tr.find('td', class_='list_click').string
 
-        data.append({
-            'num': num,
-            'title': title,
-            'writer': writer,
-            'tm': tm,
-            'viewCnt': viewCnt
-        })
+        writed_at = datetime.today().strftime('%Y-%m-%d ') + list_date
 
-    copy = soup.find('div', id='copy').prettify()
-    sitename = soup.find('span', class_='sitename').string
+        data.append((
+            1,
+            title,
+            '',
+            url,
+            '',
+            0,
+            view_cnt,
+            comment_cnt,
+            writer,
+            writed_at
+        ))
 
-    return {
-        'statusCode': 200,
-        'copy': copy,
-        'body': sitename,
-        'data': data,
-    }
+    sql = "insert into posts (site,title,description,url,image,like_cnt,view_cnt,comment_cnt,writer, writed_at) values (%s, %s, %s,%s, %s, %s,%s, %s, %s, %s)"
+
+    # print(data)
+    curs.executemany(sql, data)
+    conn.commit()
+    curs.close()
+    conn.close()
+
+    return {'statusCode': 200}
 
 
 if __name__ == '__main__':
